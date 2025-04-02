41a5adc32ea498b1a58951ceb098f8bb66360b6c
Willer Bartolomeu <153136922+WillBart21@users.noreply.github.com>
2025-04-02 20:53:54 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,15 +22,21 @@ var loaded = false;
 var init = function () {
     if (loaded) return;
     loaded = true;
-    var mobile = window.isDevice;
-    var koef = mobile ? 0.5 : 1;
     var canvas = document.getElementById('heart');
     var ctx = canvas.getContext('2d');
-    var width = canvas.width = koef * innerWidth;
-    var height = canvas.height = koef * innerHeight;
+    
+    function resizeCanvas() {
+        var mobile = window.innerWidth <= 768; // Define mobile quando a tela for menor que 768px
+        var koef = mobile ? 0.5 : 1;
+        canvas.width = koef * window.innerWidth;
+        canvas.height = koef * window.innerHeight;
+    }
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+    
     var rand = Math.random;
     ctx.fillStyle = "rgba(0,0,0,1)";
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     var heartPosition = function (rad) {
         return [Math.pow(Math.sin(rad), 3), -(15 * Math.cos(rad) - 5 * Math.cos(2 * rad) - 2 * Math.cos(3 * rad) - Math.cos(4 * rad))];
@@ -39,17 +45,10 @@ var init = function () {
         return [dx + pos[0] * sx, dy + pos[1] * sy];
     };
 
-    window.addEventListener('resize', function () {
-        width = canvas.width = koef * innerWidth;
-        height = canvas.height = koef * innerHeight;
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctx.fillRect(0, 0, width, height);
-    });
-
-    var traceCount = mobile ? 20 : 50;
+    var traceCount = window.isDevice ? 20 : 50;
     var pointsOrigin = [];
     var i;
-    var dr = mobile ? 0.3 : 0.1;
+    var dr = window.isDevice ? 0.3 : 0.1;
     for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 210, 13, 0, 0));
     for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 150, 9, 0, 0));
     for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 90, 5, 0, 0));
@@ -59,15 +58,15 @@ var init = function () {
     var pulse = function (kx, ky) {
         for (i = 0; i < pointsOrigin.length; i++) {
             targetPoints[i] = [];
-            targetPoints[i][0] = kx * pointsOrigin[i][0] + width / 2;
-            targetPoints[i][1] = ky * pointsOrigin[i][1] + height / 2;
+            targetPoints[i][0] = kx * pointsOrigin[i][0] + canvas.width / 2;
+            targetPoints[i][1] = ky * pointsOrigin[i][1] + canvas.height / 2;
         }
     };
 
     var e = [];
     for (i = 0; i < heartPointsCount; i++) {
-        var x = rand() * width;
-        var y = rand() * height;
+        var x = rand() * canvas.width;
+        var y = rand() * canvas.height;
         e[i] = {
             vx: 0,
             vy: 0,
@@ -93,7 +92,7 @@ var init = function () {
         pulse((1 + n) * .5, (1 + n) * .5);
         time += ((Math.sin(time)) < 0 ? 9 : (n > 0.8) ? .2 : 1) * config.timeDelta;
         ctx.fillStyle = "rgba(0,0,0,.1)";
-        ctx.fillRect(0, 0, width, height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         for (i = e.length; i--;) {
             var u = e[i];
             var q = targetPoints[u.q];
